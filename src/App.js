9916072b3d87dbbaea1964a094294a00f0b0baad
Ojo Oluwasetemi <setemiojo@gmail.com>
2021-11-04 16:16:30 +0100

    sample working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import "./styles.css";
-import { Link } from "react-router-dom";
+import './styles.css';
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2 onClick={() => (window.location.href = 'hello.html')}>
+        Start editing to see some magic happen!
+      </h2>
       <a href="./hello.html">Click here</a>
     </div>
   );
